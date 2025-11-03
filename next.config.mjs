ed586663b86f7b62bcf8e5339f68a526cdbfe07d
sinhdd <sinhdd@cpc.vn>
2025-11-03 12:02:13 +0700

    publish

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: 'export',
+  trailingSlash: true,
   images: {
-    unoptimized: true,
+    unoptimized: true
   },
-}
+  basePath: '/fsb-app',
+  assetPrefix: '/fsb-app/'
+};
 
-export default nextConfig
+export default nextConfig;

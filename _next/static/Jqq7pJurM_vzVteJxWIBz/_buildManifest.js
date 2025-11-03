7591158ed8d80d55b6aaf7367d4bd4c266ad26c6
sinhdd <sinhdd@cpc.vn>
2025-11-03 13:33:04 +0700

    Updates

--- _next/static/Jqq7pJurM_vzVteJxWIBz/_buildManifest.js
+++ _next/static/Jqq7pJurM_vzVteJxWIBz/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/fsb-app//_next/:path+",
-        "destination": "/fsb-app/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
